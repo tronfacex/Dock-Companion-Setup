e0eb49b576ed7ace805da9eb4a11142d85b6d377
tronfacex <50465668+tronfacex@users.noreply.github.com>
2023-01-24 00:28:13 -0600

    Add project files.

---
+++ DockCompanionConfigSetup/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace DockCompanionConfigSetup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //RetrieveWindowHandles retrieveWindowHandles;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void GetWindows_Click(object sender, RoutedEventArgs e)
+        {
+            RetrieveWindowHandles.GetWindowHandles();
+            WindowTitles.ItemsSource = RetrieveWindowHandles.WindowList;
+        }
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                inputFilePath.Text = openFileDialog.FileName;
+            }
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            string inputStringValue = inputString.Text.Replace("System.Windows.Controls.TextBox: ", "");
+            string inputFilePathValue = inputFilePath.Text.Replace("System.Windows.Controls.TextBox: ", "");
+            string filePath = "Config.txt";
+
+            if (string.IsNullOrWhiteSpace(inputStringValue) || string.IsNullOrWhiteSpace(inputFilePathValue))
+            {
+                MessageBox.Show("One or more of the required fields above is empty. Please double check both fields and try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Create the file if it doesn't exist
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Dispose();
+            }
+
+            // Write the strings to the file
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(inputStringValue);
+                writer.WriteLine(inputFilePathValue);
+            }
+          //  MessageBox.Show("Config.txt file setup complete. This application will now close.", "Config Setup", MessageBoxButton.OK);
+            MessageBoxResult result = MessageBox.Show("Config.txt file setup complete. You can now exit the Config Setup application.", "Config Setup", MessageBoxButton.OK);
+            if (result == MessageBoxResult.OK)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ DockCompanionConfigSetup/RetrieveWindowHandles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockCompanionConfigSetup
+{
+    public static class RetrieveWindowHandles
+    {
+        public static IDictionary<IntPtr, string> WindowList { get; set; }
+        public static void GetWindowHandles()
+        {
+            WindowList = OpenWindowGetter.GetOpenWindows();
+            /*foreach (KeyValuePair<IntPtr, string> window in OpenWindowGetter.GetOpenWindows())
+            {
+                IntPtr handle = window.Key;
+                string title = window.Value;
+
+                Console.WriteLine("{0}: {1}", handle, title);
+            }*/
+        }
+    }
+}
