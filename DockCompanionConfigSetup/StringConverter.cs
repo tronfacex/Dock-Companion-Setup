afe42cb2815ae72ccb9e0216b26e3c2b19804bab
tronfacex <50465668+tronfacex@users.noreply.github.com>
2023-01-25 23:08:02 -0600

    Attempting to have ListBox display window title and ProcessName

---
+++ DockCompanionConfigSetup/StringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DockCompanionConfigSetup
+{
+    public class StringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Code to replace the comma with a space
+            return value.ToString().Replace(",", " ");
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
